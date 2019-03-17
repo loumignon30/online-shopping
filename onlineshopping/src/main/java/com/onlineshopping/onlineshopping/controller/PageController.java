c74a9ff715546d7a10c72d5d82ef997f84274001
Yves Mpunga <yves.mpunga33@gmail.com>
2019-03-17 08:57:41 +0000

    Changes after master page working with dispatcher page 17/03/2019 -- 8:57

--- onlineshopping/src/main/java/com/onlineshopping/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/onlineshopping/onlineshopping/controller/PageController.java
@@ -1,22 +1,41 @@
 package com.onlineshopping.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-
-	@RequestMapping(value = { "/", "/home", "/index" })
+	 @RequestMapping(value = { "/", "/home", "/index", })
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		
+		return mv;
+
+	}
+
+//	
+	@RequestMapping(value = { "/about" })
+	public ModelAndView about() {
+
+		ModelAndView mv = new ModelAndView("about");
+		mv.addObject("title", "About us");
+		mv.addObject("userClickAbout", true);
+
 		return mv;
 	}
 
-	
+	@RequestMapping(value = { "/contact" })
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("contact");
+		mv.addObject("title", "Contact us");
+		mv.addObject("userClickContact", true);
+
+		return mv;
+	}
 
 }
